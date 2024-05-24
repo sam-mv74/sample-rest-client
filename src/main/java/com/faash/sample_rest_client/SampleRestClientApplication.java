4e81b8bffe2734d2857ff112db9ad4ea7ac6be63
sam.mv74 <sam.mv74@gmail.com>
2024-05-25 01:42:53 +0330

    Initial commit

---
+++ src/main/java/com/faash/sample_rest_client/SampleRestClientApplication.java
@@ -0,0 +1,13 @@
+package com.faash.sample_rest_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestClientApplication.class, args);
+	}
+
+}
