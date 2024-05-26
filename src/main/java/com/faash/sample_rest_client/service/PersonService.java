cff24da981fdb112717c99498d563d3772557571
s.movahedi <sam.mv74@gmail.com>
2024-05-26 16:22:38 +0330

    handle service methods exceptions

--- src/main/java/com/faash/sample_rest_client/service/PersonService.java
+++ src/main/java/com/faash/sample_rest_client/service/PersonService.java
@@ -1,8 +1,10 @@
 package com.faash.sample_rest_client.service;
 
 import com.faash.sample_rest_client.dto.PersonDTO;
+import com.faash.sample_rest_client.model.ResponseModel;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -14,18 +16,29 @@ public class PersonService {
         restTemplate = new RestTemplate();
     }
 
-    public ResponseEntity<Void> addPerson(PersonDTO personDTO, String customHeaderValue) {
+    public ResponseEntity<ResponseModel> addPerson(PersonDTO personDTO, String customHeaderValue) {
         String url = baseUrl + "/add-person";
         HttpHeaders headers = new HttpHeaders();
         headers.set("sample", customHeaderValue);
         HttpEntity<PersonDTO> requestEntity = new HttpEntity<>(personDTO, headers);
-        ResponseEntity<Void> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Void.class);
+        ResponseEntity<ResponseModel> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, ResponseModel.class);
         return response;
     }
 
-    public ResponseEntity<PersonDTO> getPersonById(Integer id) {
+    public ResponseEntity<ResponseModel> getPersonById(Integer id) {
         String url = baseUrl + "/get-person/" + id;
-        ResponseEntity<PersonDTO> response = restTemplate.getForEntity(url, PersonDTO.class);
-        return response;
+        ResponseEntity<ResponseModel> response;
+        try {
+            response = restTemplate.getForEntity(url, ResponseModel.class);
+            return response;
+        }catch (HttpServerErrorException e ){
+            ResponseModel responseModel = new ResponseModel();
+            responseModel.setSuccess(false);
+            String message = e.getStatusCode().value() == 400 ? "Bad Request" : "Internal Server Error";
+            responseModel.setMessage(message);
+            responseModel.setResponseCode(String.valueOf((e.getStatusCode().value())));
+            HttpStatus httpStatus = e.getStatusCode().value() == 400 ? HttpStatus.BAD_REQUEST : HttpStatus.INTERNAL_SERVER_ERROR;
+            return new ResponseEntity<>(responseModel,httpStatus);
+        }
     }
 }
